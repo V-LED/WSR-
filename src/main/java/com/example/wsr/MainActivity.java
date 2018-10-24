8e6cc65f16b5c8a9b007ef48ed412099ddceb1e4
V-LED <43497573+V-LED@users.noreply.github.com>
2018-10-24 15:48:24 +0700

    Add files via upload

---
+++ src/main/java/com/example/wsr/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.wsr;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        TextView headerView = (TextView) findViewById(R.id.header);
+        switch (id) {
+            case R.id.ListNews:
+                headerView.setText("Новости");
+                return true;
+            case R.id.Profile:
+                headerView.setText("Профиль");
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
